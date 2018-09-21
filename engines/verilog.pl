4d2236af62b89c246d370099df0b8e4d9fa55f07
fiveladdercon <fiveladdercon@gmail.com>
2018-09-21 12:57:45 -0400

    Verilog Modules looking good

--- engines/verilog.pl
+++ engines/verilog.pl
@@ -3,12 +3,32 @@
 #-------------------------------------------------------------------------------
 
 use EngineAPI;
-use EngineHelp;
+use EngineUtils;
+use Verilog::Module;
 use strict;
 
 #-------------------------------------------------------------------------------
 # Functions
 #-------------------------------------------------------------------------------
+
+sub collect {
+	my $region  = shift;
+	my $regions = shift;
+	my $fields  = shift;
+	while (my $node = $region->sc_get_next_child) {
+		if ($node->sc_is_field) {
+			push @{$fields}, $node;
+		} elsif ($node->sc_is_typed and not $node->sc_has_property("verilog:import")) {
+			push @{$regions}, $node;
+		} else {
+			&collect($node,$regions,$fields);
+		}
+	}
+}
+
+sub list {
+	foreach my $node (@_) { printf "%s\n", $node->sc_get_identifier(); }
+}
 
 #-------------------------------------------------------------------------------
 # Command Line
@@ -16,6 +36,31 @@ use strict;
 
 while (@ARGV) {
 	my $op = shift;
-	if    ($op eq "--help") { &help; }
-	elsif ($op eq "-h"    ) { &help; }
+	if ($op =~ /^-?-h(elp)?$/) { &uhelp; }
 }
+
+my @REGIONS = ();
+my @FIELDS  = ();
+
+&collect(&sc_get_space(),\@REGIONS,\@FIELDS);
+
+&list(@REGIONS);
+&list(@FIELDS);
+
+
+my $Module = new Module("bobo");
+
+my $Block = $Module->block();
+my $clk   = $Block->clock("clk");
+my $rst   = $Block->reset("rst_n");
+my $d     = $Block->reg("d")->clocked($clk,$rst)->output;
+my $a     = $Block->wire("a")->input;
+my $b     = $Block->wire("b")->input;
+my $sel   = $Block->wire("sel")->input;
+my $q     = $Block->wire("q");
+
+$Block->assign($q,"$sel ? $a : $b");
+
+$Block->always($d,$q);
+
+print $Module->implementation;
